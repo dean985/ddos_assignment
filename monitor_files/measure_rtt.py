7c246802cc47c2e1cbe98fe1505985498e1b85b1
Dean <carmidean@gmail.com>
2020-12-06 22:29:44 +0100

    measuring monitor is klaar

---
+++ monitor_files/measure_rtt.py
@@ -0,0 +1,48 @@
+import sys
+import shlex  
+from subprocess import Popen, PIPE, STDOUT
+from time import sleep
+def get_simple_cmd_output(cmd, stderr=STDOUT):
+    """
+    Execute a simple external command and get its output.
+    """
+    args = shlex.split(cmd)
+    return Popen(args, stdout=PIPE, stderr=stderr,text=True).communicate()[0]
+
+def get_ping_time(host):
+    host = host.split(':')[0]
+    cmd = "fping {host} -C 3 -q".format(host=host)
+    res = [float(x) for x in get_simple_cmd_output(cmd).strip().split(':')[-1].split() if x != '-']
+    if len(res) > 0:
+        return sum(res) / len(res)
+    else:
+        return 999999
+
+
+def main(dest, program_type):
+    f = None
+    if program_type== 'c':
+        f = open('pings_results_c.txt', 'a')
+    else:
+        f = open('pings_results_p.txt', 'a')        
+    pings_checks = 0
+    avg = 0
+    while(True):
+        pings_checks +=1
+        rtt_seconds = get_ping_time(dest)
+        f.write(f'{pings_checks}, {rtt_seconds}\n')
+        avg += rtt_seconds
+        sleep(5)
+    avg /= pings_checks
+    f.write(f'Average: {avg}')
+    f.close()
+
+# usage : python3.8 measure_rtt.py <IP> <c/p>
+if len(sys.argv) != 3:
+    print('usage : python3.8 measure_rtt.py <IP> <c/p>\nc/p - p for python, c for C\n')
+else:
+    main(sys.argv[1], sys.argv[2])
+    
+
+
+
